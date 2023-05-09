9bc720ae4e827abfa2507716c9dca670e1e820e4
trolidichvu <120279614+trolidichvu@users.noreply.github.com>
2023-05-09 23:26:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { spawn } = require('child_process');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/command', (req, res) => {
+  const { command } = req.body;
+  executeCommand(command, (output) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(output);
+  });
+});
+
+function executeCommand(command, callback) {
+  const [cmd, ...args] = command.split(/\s+/);
+  const childProcess = spawn(cmd, args);
+  let output = '';
+  childProcess.stdout.on('data', (data) => {
+    output += data;
+  });
+  childProcess.stderr.on('data', (data) => {
+    output += data;
+  });
+  childProcess.on('exit', (code) => {
+    callback(output);
+  });
+}
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
